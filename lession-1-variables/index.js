0446b16f41c582c90cbfe39a25b3e4401b600790
Duong Nguyen <dnguyenlearning@gmail.com>
2022-08-12 10:29:50 -0400

    more

--- lession-1-variables/index.js
+++ lession-1-variables/index.js
@@ -93,54 +93,88 @@ const number = 10000000;
 // console.log("person", person);
 // console.log("person2", person2);
 
-let arrayObjects = [
-  { id: 1, name: "vai thun", test: { hehe: "vu" }, tien: 100000 },
-  { id: 2, name: "vai thieu", test: { hehe: "hao" }, tien: 200000 },
-  { id: 3, name: "cotton" },
-  { id: 4, name: "ao dai" },
-];
+// let arrayObjects = [
+//   { id: 1, name: "vai thun", test: { hehe: "vu" }, tien: 100000 },
+//   { id: 2, name: "vai thieu", test: { hehe: "hao" }, tien: 200000 },
+//   { id: 3, name: "cotton" },
+//   { id: 4, name: "ao dai" },
+// ];
 
 //result
 
-let a = {
-  1: { id: 1, name: "vai thun", test: { hehe: "vu" }, tien: 100000 },
-  2: { id: 2, name: "vai thieu", test: { hehe: "hao" }, tien: 200000 },
-};
+// let a = {
+//   1: { id: 1, name: "vai thun", test: { hehe: "vu" }, tien: 100000 },
+//   2: { id: 2, name: "vai thieu", test: { hehe: "hao" }, tien: 200000 },
+// };
 
-let array = [12, 3, 2, 1, 2, 34];
+// let array = [12, 3, 2, 1, 2, 34];
 
-console.log("includes", array?.includes(1));
-console.log(
-  "some",
-  array?.some((number) => number === 11)
-);
+// console.log("includes", array?.includes(1));
+// console.log(
+//   "some",
+//   array?.some((number) => number === 11)
+// );
 
-const nameIsVu = arrayObjects?.filter((obj) => obj?.name?.length > 7);
+// const nameIsVu = arrayObjects?.filter((obj) => obj?.name?.length > 7);
 
 // const mapArray = array?.map((number) => ({
 //   value: number,
 //   lable: `string ${number + 1}`,
 // }));
 
-const total = arrayObjects?.reduce((t, obj) => {
-  return t + (obj?.tien || 0);
-}, 0);
+// const total = arrayObjects?.reduce((t, obj) => {
+//   return t + (obj?.tien || 0);
+// }, 0);
 
-console.log(
-  "findIndex",
-  arrayObjects.findIndex((item) => item.id === 5)
-);
+// console.log(
+//   "findIndex",
+//   arrayObjects.findIndex((item) => item.id === 5)
+// );
 
-console.log(
-  "find",
-  arrayObjects.find((item) => item.id === 3)
-);
+// console.log(
+//   "find",
+//   arrayObjects.find((item) => item.id === 3)
+// );
 
-const mapArrayObjects = arrayObjects?.reduce((t, obj) => {
-  t[obj.id] = obj;
-  return t;
-}, {});
+// const mapArrayObjects = arrayObjects?.reduce((t, obj) => {
+//   t[obj.id] = obj;
+//   return t;
+// }, {});
 
-console.log("mapArrayObjects", mapArrayObjects);
+// console.log("mapArrayObjects", mapArrayObjects);
 
-console.log("mapArrayObjects", mapArrayObjects?.[1]);
+// console.log("mapArrayObjects", mapArrayObjects?.[1]);
+
+// const num = 3.34;
+
+// const rounded = Math.round(num);
+// const ceil = Math.ceil(num);
+// const floor = Math.floor(num);
+// const trunc = Math.trunc(num);
+// console.log("rounded", rounded);
+// console.log("ceil", ceil);
+// console.log("floor", floor);
+// console.log("trunc", trunc);
+
+// let a = 1;
+// let b = 2;
+
+// const max = Math.max(a, b);
+// const min = Math.min(a, b);
+
+// console.log("max", max);
+// console.log("min", min);
+
+// const arr = [1, 2, 10, 200, 100];
+
+// const maxInArr = Math.max(...arr);
+// const minInArr = Math.min(...arr);
+
+// console.log("maxInArr", maxInArr);
+// console.log("minInArr", minInArr);
+
+// const random = Math.random();
+
+// console.log("random", random);
+
+// const randomNumFrom1To20 = 1 + Math.floor(Math.random() * 20);
---
+++ lession-2-operator/index.js
@@ -0,0 +1,72 @@
+// console.log("==", 1 == 1);
+// console.log("==", 1 != 1);
+// console.log("==", 1 == "1");
+
+// console.log("===", 1 === "1");
+
+// if (1 > 2) {
+//   console.log("1>2");
+// } else if (2 > 3) {
+//   console.log("2>1");
+//   //...
+// } else {
+// }
+
+// let type = "somethi1ng";
+
+// switch (type) {
+//   case "something":
+//     console.log("something");
+//     break;
+//   case "hehe":
+//     console.log("hehe");
+//     break;
+// }
+
+// console.log("end");
+
+// const arr = [...Array(10).keys()]?.map((key) => key + 10);
+
+// for (let i = 0; i < arr?.length; i++) {
+//   if (arr[i] > 6) {
+//     console.log("stop");
+//     break;
+//   }
+
+//   console.log("run value", arr[i]);
+// }
+
+// const object = {
+//   test: 1,
+//   test1: 2,
+//   test3: 1,
+//   test4: 1,
+//   test5: 1,
+// };
+
+// for (const [index, value] of Object.entries(object)) {
+//   console.log("index", index);
+//   console.log("value", value);
+// }
+
+// console.log("end");
+
+// const arr = [1, 2, 21, 3, 1, 2, 3, 4, 5, 6, 7];
+
+// const uniqueValues = [...new Set(arr)];
+
+// console.log("uniqueValues", uniqueValues);
+
+// typeof 1; //number
+// typeof "hehe"; //string
+// typeof { test: "123" }; //object
+// typeof [1, 2]; //object;
+
+// console.log("typeof []", typeof [1, 2]);
+
+// console.log("is Array?", Array.isArray([1, 2]));
+
+let y = "5"; // y is a string
+let x = +y; // x is a number
+
+console.log("x", x);
